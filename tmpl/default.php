7213131adb9705ea550a40662e901ef98ee02a54
Matt Thomas <matt@betweenbrain.com>
2014-01-17 12:34:19 -0500

    Adds 2.5 Hello Ajax World module

--- tmpl/default.php
+++ tmpl/default.php
@@ -2,9 +2,9 @@
 
 /**
  * File       default.php
- * Created    5/22/13 6:43 AM
+ * Created    1/17/14 12:29 PM
  * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
- * Support    https://github.com/betweenbrain/Ajax-Session-Module/issues
+ * Support    https://github.com/Joomla-Ajax-Interface/Hello-Ajax-World-Module/issues
  * Copyright  Copyright (C) 2013 betweenbrain llc. All Rights Reserved.
  * License    GNU General Public License version 2, or later.
  */
